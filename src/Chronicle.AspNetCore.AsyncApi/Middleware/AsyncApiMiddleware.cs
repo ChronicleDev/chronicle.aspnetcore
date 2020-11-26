40de963a4308f93eebe8d4adcb05c4b604c59e06
Dom Knight <dom.knight@dkk-contracting.com>
2020-11-26 17:47:04 +0000

    Stub middleware

---
+++ src/Chronicle.AspNetCore.AsyncApi/Middleware/AsyncApiMiddleware.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace Chronicle.AspNetCore.AsyncApi.Middleware
+{
+    public class AsyncApiMiddleware
+    {
+        public Task Invoke(HttpContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
