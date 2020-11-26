2e02c46092581a39ae4371a1c904ce9655e8b58a
Dom Knight <dom.knight@dkk-contracting.com>
2020-11-26 17:39:47 +0000

    Add stub extension method

--- src/Chronicle.AspNetCore.AsyncApi/Extensions/ApplicationBuilderExtensions.cs
+++ src/Chronicle.AspNetCore.AsyncApi/Extensions/ApplicationBuilderExtensions.cs
@@ -1,10 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Microsoft.AspNetCore.Builder;
 
 namespace Chronicle.AspNetCore.AsyncApi.Extensions
 {
     public static class ApplicationBuilderExtensions
     {
+        public static IApplicationBuilder UseAsyncApi(this IApplicationBuilder app)
+        {
+            return app;
+        }
     }
 }
